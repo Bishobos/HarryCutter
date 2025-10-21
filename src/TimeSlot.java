a65a8f66ae7c8c24cab1cb65bf8c584feb587191
Bishobos <mps.kajkage@gmail.com>
2025-10-21 10:42:32 +0200

    TimeSlot class made

--- src/TimeSlot.java
+++ src/TimeSlot.java
@@ -1,10 +1,40 @@
-public class TimeSlot implements Comparable<TimeSlot> {
-    int day;
-    int month;
-    int timestamp;
-    String name;
-    boolean paid;
+/**
+ * TimeSlot er en... <br>
+ * Has methods: <br>
+ *
+ * Has attributes: <br>
+ *
+ */
 
+public class TimeSlot implements Comparable<TimeSlot> {
+    /**
+     * day er en...
+     */
+    private final int day;
+    /**
+     * month er en...
+     */
+    private final int month;
+    /**
+     * timestamp er en...
+     */
+    private final int timestamp;
+    /**
+     * name er en...
+     */
+    private String name;
+    /**
+     * paid er en...
+     */
+    private boolean paid;
+
+    /**
+     * Timeslot constructor for new bookings
+     * @param day
+     * @param month
+     * @param timestamp
+     * @param name
+     */
     public TimeSlot(int day, int month, int timestamp, String name){
         this.day = day;
         this.month = month;
@@ -13,15 +43,77 @@ public class TimeSlot implements Comparable<TimeSlot> {
         this.paid = false;
     }
 
-    //Setters and getters
-
-    @Override
-    public String toString(){
-        return "";
+    /**
+     * Timeslot constructor for reading bookings from file
+     * @param day
+     * @param month
+     * @param timestamp
+     * @param name
+     */
+    public TimeSlot(int day, int month, int timestamp, String name, boolean isPaid){
+        this.day = day;
+        this.month = month;
+        this.timestamp = timestamp;
+        this.name = name;
+        this.paid = isPaid;
     }
 
+    /**
+     * getTime is a...
+     * @return
+     */
+    public int getTime(){
+        return Integer.parseInt(String.valueOf(this.day)
+                + String.valueOf(this.month)
+                + String.valueOf(this.timestamp));
+    }
+
+    /**
+     * getDay..
+     * @return
+     */
+    public int getDay(){return this.day;}
+
+    /**
+     * getMonth
+     * @return
+     */
+    public int getMonth(){return this.month;}
+
+    /**
+     * getTimestamp
+     * @return
+     */
+    public int getTimestamp(){return this.timestamp;}
+
+    /**
+     * getName
+     * @return
+     */
+    public String getName(){return this.name;}
+
+    /**
+     * setName
+     * @param newName
+     */
+    public void setName(String newName){this.name = newName;}
+
+    /**
+     * toString override
+     * @return
+     */
+    @Override
+    public String toString(){
+        return this.day + "" + this.month + " " + this.timestamp + "\n" + this.name;
+    }
+
+    /**
+     * compareTo comparable interface override
+     * @param t
+     * @return
+     */
     @Override
     public int compareTo(TimeSlot t){
-        return 0;
+        return (this.getTime() - t.getTime());
     }
 }
