ab8cc953545dadeb8299f2b4568c302467e95f54
Bishobos <mps.kajkage@gmail.com>
2025-10-22 11:11:41 +0200

    documentation added

--- src/FileHandler/ReadAppointments.java
+++ src/FileHandler/ReadAppointments.java
@@ -6,10 +6,25 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 
+/**
+ * ReadAppointments er en klasse der bruges til at læse Appointments filer. Den implementerer {@link CSVReader} interfacet
+ * Has Methods:
+ * {@link #reader(String)}
+ * {@link #format(String)}
+ * Has Attributes:
+ * {@link #formatedData}
+ */
 public class ReadAppointments implements CSVReader{
-
+    /**
+     * formatedData er en ArrayList&ltString[]&gt af data læst fra filen
+     */
     ArrayList<String[]> formatedData = new ArrayList<>();
 
+    /**
+     * reader er hovedmetoden der bruges til at læse data fra filer
+     * @param filename navnet på filen uden path eller extention {@link #setFilename(String)} tilføjer det
+     * @return {@link #formatedData} nu fyldt med læst og formatered data
+     */
     @Override
     public ArrayList<String[]> reader(String filename){
         String formatedFilename = setFilename(filename);
@@ -27,11 +42,21 @@ public class ReadAppointments implements CSVReader{
         return formatedData;
     }
 
+    /**
+     * format bruges til at splitte læste linjer med ; som delimiter
+     * @param input er den String der skal splittes
+     * @return String[] af splittet input
+     */
     @Override
-    public String[] format(String test){
-        String[] result = test.split(";");
+    public String[] format(String input){
+        String[] result = input.split(";");
         return result;
     }
 
+    /**
+     * setFilename formaterer filename med path og extention
+     * @param filename uformateret fil navn
+     * @return String fil navn med path og exctention
+     */
     private String setFilename(String filename){return "src/FileHandler/Files/Appointments/" + filename + ".csv";}
 }
