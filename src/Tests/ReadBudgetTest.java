9f9814f6e25ad5e25a66751b343cd056d5f38a2c
Bishobos <mps.kajkage@gmail.com>
2025-10-22 20:07:04 +0200

    ReadProductSales works

--- src/Tests/ReadBudgetTest.java
+++ src/Tests/ReadBudgetTest.java
@@ -1,9 +1,13 @@
 package Tests;
 import FileHandler.ReadBudget;
 
+import java.util.Arrays;
+
 public class ReadBudgetTest {
     public static void main(String[] args){
         ReadBudget budget = new ReadBudget();
-        System.out.println(budget.reader().getLast()[5]);
+        for (String[] strings : budget.reader()) {
+            System.out.println(Arrays.toString(strings));
+        }
     }
 }
