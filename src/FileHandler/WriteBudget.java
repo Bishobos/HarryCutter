692ecdd1d09d5909f7fd4090ee076545e56de355
Bishobos <mps.kajkage@gmail.com>
2025-10-22 21:49:28 +0200

    WriteBudget works, WriteBudgetTest will currently add duplicate entries that need to be removed manually. 22;10;2025;1100;Test;0.0 is the test entry

--- src/FileHandler/WriteBudget.java
+++ src/FileHandler/WriteBudget.java
@@ -2,16 +2,39 @@ package FileHandler;
 
 import Main.TimeSlot;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class WriteBudget implements CSVWriter {
+    double amountPaid;
+    String filename = "src/FileHandler/Files/Appointments/budget.csv";
+    TimeSlot toWrite;
+
+    public WriteBudget(TimeSlot toWrite, double amountPaid){this.toWrite = toWrite; this.amountPaid = amountPaid;}
 
     @Override
     public void writer(){
+        try(BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(this.filename, true))){
+        bufferedWriter.write(format(this.toWrite));
+        bufferedWriter.newLine();
+        }catch (IOException e) {
+            throw new RuntimeException(e);
+    }
 
     }
 
     @Override
     public String format(TimeSlot toFormat){
-        return "formatted export String";
+        String day = Integer.toString(toFormat.getDay());
+        String month = Integer.toString(toFormat.getMonth());
+        String year = Integer.toString(toFormat.getYear());
+        String timestamp = Integer.toString(toFormat.getTimestamp());
+        String name = toFormat.getName();
+        String paid = Double.toString(this.amountPaid);
+
+        return day + ";" + month + ";" + year + ";" + timestamp + ";" + name + ";" + paid;
+
     }
     //may need parameters both here and in interface, maybe of type schedule
 }
