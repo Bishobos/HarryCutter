901c56008c3300579e2c77dc328288cf1b71842a
Bishobos <mps.kajkage@gmail.com>
2025-10-22 22:58:19 +0200

    ReadBudget now asks for a boolean indication if a password has been entered, throws error if not

--- src/FileHandler/ReadBudget.java
+++ src/FileHandler/ReadBudget.java
@@ -4,12 +4,23 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.rmi.AccessException;
 import java.util.ArrayList;
 
 public class ReadBudget implements CSVReader{
 
     ArrayList<String[]> formatedData = new ArrayList<>();
     String filename = "src/FileHandler/Files/Appointments/budget.csv";
+
+    public ReadBudget(boolean isPasswordValidated){
+        if (!isPasswordValidated) {
+            try {
+                throw new IllegalAccessException("Password required!");
+            } catch (IllegalAccessException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
 
     @Override
     public ArrayList<String[]> reader(){
@@ -39,9 +50,5 @@ public class ReadBudget implements CSVReader{
     }
 
     //may need parameters both here and in interface
-
-    public boolean inputPassword(){
-        return false;
-    }
 
 }
--- src/Tests/ReadBudgetTest.java
+++ src/Tests/ReadBudgetTest.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 
 public class ReadBudgetTest {
     public static void main(String[] args){
-        ReadBudget budget = new ReadBudget();
+        ReadBudget budget = new ReadBudget(false);
         for (String[] strings : budget.reader()) {
             System.out.println(Arrays.toString(strings));
         }
