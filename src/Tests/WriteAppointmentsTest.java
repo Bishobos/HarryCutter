c3f2ccd2dcf159e657a3e2c785a53a590811a557
Bishobos <mps.kajkage@gmail.com>
2025-10-23 19:46:51 +0200

    fixed to suit new AppointmentHandler constructor

--- src/Tests/WriteAppointmentsTest.java
+++ src/Tests/WriteAppointmentsTest.java
@@ -5,7 +5,7 @@ import Main.AppointmentHandler;
 
 public class WriteAppointmentsTest {
     public static void main(String[] args){
-        AppointmentHandler handler = new AppointmentHandler();
+        AppointmentHandler handler = new AppointmentHandler("222025");
         WriteAppointments writer = new WriteAppointments(handler);
         writer.writer();
     }
